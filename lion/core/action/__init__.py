15040bb88cc2422ce539d990f86a5d9a23c64076
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2024-11-13 13:44:27 -0500

    renamed tool_manager to action manager

--- lion/core/action/__init__.py
+++ lion/core/action/__init__.py
@@ -1,12 +1,12 @@
+from .action_manager import ActionManager
 from .base import ObservableAction
 from .function_calling import FunctionCalling
 from .tool import Tool, func_to_tool
-from .tool_manager import ToolManager
 
 __all__ = [
     "ObservableAction",
     "FunctionCalling",
     "Tool",
     "func_to_tool",
-    "ToolManager",
+    "ActionManager",
 ]
